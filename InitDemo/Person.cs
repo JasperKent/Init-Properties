ee03721fc4e80a63937b94a789b285af3ef2bff9
Jasper Kent <jasper_kent@hotmail.com>
2020-12-18 09:12:45 +0000

    Add project files.

---
+++ InitDemo/Person.cs
@@ -0,0 +1,37 @@
+﻿namespace InitDemo
+{
+    class Person
+    {
+        private readonly int _age;
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+
+            init
+            {
+                _age = value;
+            }
+        }
+
+       // public int Age { get; init; }
+
+        public Person(int age)
+        {
+            Age = age;
+        }
+
+        public Person()
+        {
+
+        }
+  
+        public void SomeFunc()
+        {
+          //  Age = 50;
+        }
+    }
+}
---
+++ InitDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person { Age = 27 };
+
+          //  p.Age = 50;
+
+            Console.WriteLine(p.Age);
+        }
+    }
+}
